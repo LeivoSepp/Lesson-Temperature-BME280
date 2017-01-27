643429035a0786f7235ae4f2ac97660c40df2b9c
Leivo Sepp <leivo@robootika.com>
2017-01-27 17:53:43 +0200

    Update NuGet packages

--- LessonTemperatureBME280/StartupTask.cs
+++ LessonTemperatureBME280/StartupTask.cs
@@ -5,24 +5,18 @@ using Glovebox.Graphics.Components;
 using Glovebox.IoT.Devices.Sensors;
 using System.Threading.Tasks;
 
-// The Background Application template is documented at http://go.microsoft.com/fwlink/?LinkID=533884&clcid=0x409
-
 namespace LessonTemperatureBME280
 {
     public sealed class StartupTask : IBackgroundTask
     {
-        Ht16K33 driver = new Ht16K33(new byte[] { 0x70, 0x72 }, Ht16K33.Rotate.None);
-        //BackgroundTaskDeferral _deferral;
+        Ht16K33 driver = new Ht16K33(new byte[] { 0x70 }, Ht16K33.Rotate.None);
         public void Run(IBackgroundTaskInstance taskInstance)
         {
-            //_deferral = taskInstance.GetDeferral();
             LED8x8Matrix matrix = new LED8x8Matrix(driver);
-            //BMP280 tempAndPressure = new BMP280();
             BME280 tempPressureAndHumidity = new BME280(0x76);
 
             while (true)
             {
-                //var message = $"{Math.Round(tempAndPressure.Temperature.DegreesCelsius, 1)}C, {Math.Round(tempAndPressure.Pressure.Hectopascals, 1)}hPa ";
                 var message = $"{Math.Round(tempPressureAndHumidity.Temperature.DegreesCelsius, 1)}C, {Math.Round(tempPressureAndHumidity.Pressure.Atmospheres, 1)}atm, {Math.Round(tempPressureAndHumidity.Humidity, 1)}% ";
                 matrix.ScrollStringInFromRight(message, 70);
                 Task.Delay(1000).Wait();
